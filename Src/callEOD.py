68f2a949e62ed84e152dc1af17deb8de444b34ee
Gurj33t-Singh <gurjeets1803@gmail.com>
2022-10-26 15:01:00 +0530

    backup

--- Src/callEOD.py
+++ Src/callEOD.py
@@ -3,23 +3,30 @@ from FileOps import *
 import GetConfigs
 import requests
 
+def getEod(date_fromStr, date_toStr)
 
 #create API session
 sessionObj=requests.Session()
 queryParam={
     "access_key": "3c99248d6bcca4a8fe08c99eace0691a",
     "symbols": "AAPL",
-    "date_from": "2022-04-01",
-    "date_to": "2022-04-20"
+    "date_from": "",
+    "date_to": ""
 }
+queryParam["date_from"]=
 response=sessionObj.get("http://api.marketstack.com/v1/eod", params=queryParam)
 EodDict=response.json()
 
 
 #write json from response
-Fobj=open(GetConfigs.getAbsPath("Sample"), "wt")
-json.dump(EodDict ,Fobj, indent="")
-Fobj.close()
+try:
+    Fobj=open(GetConfigs.getAbsPath("Sample"), "x")
+    json.dump(EodDict ,Fobj, indent="")
+    Fobj.close()
+except:
+    Fobj = open(GetConfigs.getAbsPath("Sample"), "w")
+    json.dump(EodDict, Fobj, indent="")
+    Fobj.close()
 
 #read created file
 Fobj1=open(GetConfigs.getAbsPath("Sample"), "rt").read()
